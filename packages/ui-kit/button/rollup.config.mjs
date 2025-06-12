036cf9ccaff2739d50ffc1980193d79444a51f44
Alexandr Yakovlev <sanya-yakovlev-95@mail.ru>
2025-06-12 14:59:19 +0300

    chore: initial project

---
+++ packages/ui-kit/button/rollup.config.mjs
@@ -0,0 +1,28 @@
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import terser from "@rollup/plugin-terser";
+import typescript from "rollup-plugin-typescript2";
+
+export default {
+  input: "index.ts",
+  output: [
+    {
+      file: "dist/index.cjs.js",
+      format: "cjs",
+    },
+    {
+      file: "dist/index.esm.js",
+      format: "esm",
+    },
+  ],
+  plugins: [
+    terser(),
+    resolve(),
+    commonjs(),
+    typescript({
+      lib: ["es2019", "dom", "es5", "es6"],
+      target: "es5",
+    }),
+  ],
+  external: ["react", "react-dom"],
+};
